e7ec3e317e4c1108087e46584dda5036609230b1
Victor A. P. Magri <50467563+victorapm@users.noreply.github.com>
2023-10-12 16:32:29 -0400

    Add MGR statistics (#897)
    This PR improves statistics reporting for MGR.
    
    A list with detailed changes is given below:
    * Add MatrixStats and MatrixStatsArray
    * Add hypre_squared utility
    * Fix divisor line location for Rectangular matrices
    * Minor fix on hypre_squared definition
    * Move nonzero variable definitions up
    * Initialize global number of nonzeros at matrix creation
    * Add hypre_ParCSRMatrixStatsArray and helper functions
    * Add par_csr_matstats_device
    * Add par_mgr_stats
    * Print F-relax data only once
    * Fix clang-13 build
    * IJ driver now passes in print_level option to MGR
    * GPU runs always require A_FF in MGR
    * Add HYPRE_PRINT_SHIFTED_PARAM macro
    * Add hypre_IntArraySetInterleavedValues (host/device implementations)
    * Fix F-relaxation reporting + refactoring
    * Update global number of nonzeros of the matrix
    * Move new BoomerAMG functions to par_stats
    * Apply astyle

--- src/test/ij.c
+++ src/test/ij.c
@@ -5287,7 +5287,7 @@ main( hypre_int argc,
          HYPRE_MGRSetGlobalSmoothType(pcg_precond, mgr_gsmooth_type);
          HYPRE_MGRSetMaxGlobalSmoothIters( pcg_precond, mgr_num_gsmooth_sweeps );
          /* set print level */
-         HYPRE_MGRSetPrintLevel(pcg_precond, 1);
+         HYPRE_MGRSetPrintLevel(pcg_precond, ioutdat);
          /* set max iterations */
          HYPRE_MGRSetMaxIter(pcg_precond, 1);
          HYPRE_MGRSetTol(pcg_precond, pc_tol);
@@ -7504,7 +7504,7 @@ main( hypre_int argc,
          HYPRE_MGRSetInterpType(pcg_precond, mgr_interp_type);
          HYPRE_MGRSetNumInterpSweeps(pcg_precond, mgr_num_interp_sweeps);
          /* set print level */
-         HYPRE_MGRSetPrintLevel(pcg_precond, 1);
+         HYPRE_MGRSetPrintLevel(pcg_precond, ioutdat);
          /* set max iterations */
          HYPRE_MGRSetMaxIter(pcg_precond, 1);
          HYPRE_MGRSetTol(pcg_precond, pc_tol);
@@ -7988,7 +7988,7 @@ main( hypre_int argc,
          HYPRE_MGRSetInterpType(pcg_precond, mgr_interp_type);
          HYPRE_MGRSetNumInterpSweeps(pcg_precond, mgr_num_interp_sweeps);
          /* set print level */
-         HYPRE_MGRSetPrintLevel(pcg_precond, 1);
+         HYPRE_MGRSetPrintLevel(pcg_precond, ioutdat);
          /* set max iterations */
          HYPRE_MGRSetMaxIter(pcg_precond, 1);
          HYPRE_MGRSetTol(pcg_precond, pc_tol);
@@ -8367,7 +8367,7 @@ main( hypre_int argc,
          HYPRE_MGRSetInterpType(pcg_precond, mgr_interp_type);
          HYPRE_MGRSetNumInterpSweeps(pcg_precond, mgr_num_interp_sweeps);
          /* set print level */
-         HYPRE_MGRSetPrintLevel(pcg_precond, 1);
+         HYPRE_MGRSetPrintLevel(pcg_precond, ioutdat);
          /* set max iterations */
          HYPRE_MGRSetMaxIter(pcg_precond, 1);
          HYPRE_MGRSetTol(pcg_precond, pc_tol);
@@ -8740,7 +8740,7 @@ main( hypre_int argc,
       HYPRE_MGRSetInterpType(mgr_solver, mgr_interp_type);
       HYPRE_MGRSetNumInterpSweeps(mgr_solver, mgr_num_interp_sweeps);
       /* set print level */
-      HYPRE_MGRSetPrintLevel(mgr_solver, 3);
+      HYPRE_MGRSetPrintLevel(mgr_solver, ioutdat);
       /* set max iterations */
       HYPRE_MGRSetMaxIter(mgr_solver, max_iter);
       HYPRE_MGRSetTol(mgr_solver, tol);
